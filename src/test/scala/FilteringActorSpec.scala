fa1e835ca9acfbe618dda3c1d859a3ccd00d4a94
Christian Kleinboelting <christian@kleinboelting.de>
2016-11-03 20:09:48 +0100

    Add FilteringActor test with expectNoMsg

--- src/test/scala/FilteringActorSpec.scala
+++ src/test/scala/FilteringActorSpec.scala
@@ -27,8 +27,31 @@ class FilteringActorSpec extends TestKit(ActorSystem("testsystem"))
         case Event(id) if id <= 5 => id
       }
 
-      eventIds must be(List(1,2,3,4,5))
+      eventIds must be(List(1, 2, 3, 4, 5))
       expectMsg(Event(6))
+    }
+
+    "filter out particular messages using expectNoMsg" in {
+      import FilteringActor._
+
+      val props = FilteringActor.props(testActor, 5)
+      val filter = system.actorOf(props, "filter-2")
+      filter ! Event(1)
+      filter ! Event(2)
+      expectMsg(Event(1))
+      expectMsg(Event(2))
+      filter ! Event(1)
+      expectNoMsg()
+      filter ! Event(3)
+      expectMsg(Event(3))
+      filter ! Event(1)
+      expectNoMsg()
+      filter ! Event(4)
+      filter ! Event(5)
+      filter ! Event(5)
+      expectMsg(Event(4))
+      expectMsg(Event(5))
+      expectNoMsg()
     }
   }
 
